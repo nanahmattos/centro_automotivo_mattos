e3f8d27a11503fc09ac6aab9112c0af7effdd50c
Carolina Mattos <mattos.carolina98@gmail.com>
2025-06-27 16:40:25 -0400

    feat: images

--- src/components/cmp_depoimentos.vue
+++ src/components/cmp_depoimentos.vue
@@ -1,156 +1,73 @@
 <template>
-  <div class="bg-grey-9">
-    <!-- <div class="row justify-between items-center">
-      <div class="q-gutter-sm">
-        <q-btn
-          flat
-          class="nav-arrow"
-          @click="prevSlide"
-          :disabled="currentIndex === 0"
-          aria-label="Slide anterior"
-          icon="keyboard_arrow_left"
-        />
-        <q-btn
-          flat
-          class="nav-arrow"
-          @click="nextSlide"
-          aria-label="Slide anterior"
-          icon="keyboard_arrow_right"
-          :disabled="currentIndex === totalSlides - 1"
-        />
-      </div>
-    </div> -->
-    <p class="text-center title-primary text-white">Serviços</p>
-
-    <q-carousel
-      v-model="slide"
-      swipeable
-      animated
-      class="bg-grey-9"
-      transition-prev="scale"
-      transition-next="scale"
-      padding
-      arrows
-      control-color="white"
-      navigation
-      height="auto"
-    >
-      <q-carousel-slide
-        v-for="(group, index) in chunkedItems"
-        :key="index"
-        :name="index"
-        class="row col-12 no-wrap"
-        style="width: 100%"
-      >
-        <q-card v-for="(item, id) in group" :key="id" class="card items-center">
-          <div class="q-pa-sm">
-            <q-img :src="item.icon" style="width: 100px" fit="contain" />
-          </div>
-          <div class="content">
-            <p class="title-secondary text-white">{{ item.title }}</p>
-            <p class="texto-primary text-white">{{ item.texto }}</p>
-          </div>
-        </q-card>
-      </q-carousel-slide>
-    </q-carousel>
+  <div>
+    <p class="text-center">O que estão dizendo ?</p>
+    <div class="card-container row">
+      <q-card v-for="(depoimento, index) in depoimentos" :key="index" class="card items-center">
+        <q-card-section>
+          <p>{{ depoimento.title }}</p>
+          <p>
+            {{ depoimento.texto }}
+          </p>
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-import { useQuasar } from 'quasar'
-import { useRoute } from 'vue-router'
-import icon_susp from '../assets/images/suspensao.svg'
-const route = useRoute()
-const $q = useQuasar()
+import { ref } from 'vue'
 
-const servicos = ref([
+const depoimentos = ref([
   {
     id: 1,
-    title: 'Serviço 1',
+    title: 'Cliente 1',
     texto:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
-    icon: icon_susp,
+    icon: '',
   },
   {
     id: 2,
-    title: 'Serviço 2',
+    title: 'Cliente 2',
     texto:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
-    icon: icon_susp,
+    icon: '',
   },
 
   {
     id: 3,
-    title: 'Serviço 3',
+    title: 'Cliente 3',
     texto:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
-    icon: icon_susp,
+    icon: '',
   },
   {
     id: 4,
-    title: 'Serviço 4',
+    title: 'Cliente 4',
     texto:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
-    icon: icon_susp,
+    icon: '',
   },
 ])
-
-const slide = ref(0)
-const filteredItems = computed(() => servicos.value.filter((item) => item.name !== route.name))
-
-const chunkSize = computed(() => {
-  if ($q.screen.xs) return 1 // até ~599px
-  if ($q.screen.sm) return 2 // 600px a 1023px
-  if ($q.screen.md) return 3 // 1024px a 1439px
-  return 4 // lg e xl
-})
-function chunk(array, size) {
-  const result = []
-  for (let i = 0; i < array.length; i += size) {
-    result.push(array.slice(i, i + size))
-  }
-  return result
-}
-
-const chunkedItems = computed(() => chunk(filteredItems.value, chunkSize.value))
-// const totalSlides = computed(() => chunkedItems.value.length)
-// const currentIndex = computed(() => slide.value)
-
-// const nextSlide = () => {
-//   if (currentIndex.value < totalSlides.value - 1) {
-//     slide.value++
-//   }
-// }
-
-// const prevSlide = () => {
-//   if (currentIndex.value > 0) {
-//     slide.value--
-//   }
-// }
 </script>
 <style scoped lang="scss">
-.card {
-  max-width: 400px;
-  width: 100%;
-  background-color: $primary;
-  cursor: pointer;
-  margin: 10px;
+.card-container {
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.card {
+  max-width: 200px;
+  width: 100%;
+  margin: 10px;
+  align-items: stretch;
   border-radius: 25px;
-  flex-direction: column;
   text-align: center;
-  overflow: hidden;
 
   .image {
     max-width: 100px;
     max-height: 200px;
     height: 100%;
     width: 100%;
-  }
-
-  .content {
-    padding: 10px;
   }
 }
 </style>
--- src/components/cmp_marcas.vue
+++ src/components/cmp_marcas.vue
@@ -6,7 +6,12 @@
       <div class="carousel-wrapper">
         <div class="carousel-track">
           <div v-for="(imagem, index) in imagensDuplicadas" :key="index" class="carousel-item">
-            <q-img :src="imagem.img" fit="contain" :alt="imagem.alt" style="height: 60px; width: 100px" />
+            <q-img
+              :src="imagem.img"
+              fit="contain"
+              :alt="imagem.alt"
+              style="height: 60px; width: 100px"
+            />
           </div>
         </div>
       </div>
@@ -16,18 +21,42 @@
 
 <script setup>
 import { ref, computed } from 'vue'
-import mahle from '../assets/images/mahle.svg'
+import img_bosch from 'src/assets/images/marcas/bosch.png'
+import img_mahle from 'src/assets/images/marcas/mahle.png'
+import img_cobreq from 'src/assets/images/marcas/cobreq.png'
+import img_cofap from 'src/assets/images/marcas/cofap.png'
+import img_controil from 'src/assets/images/marcas/controil.png'
+import img_frasle from 'src/assets/images/marcas/frasle.png'
+import img_fremax from 'src/assets/images/marcas/fremax.png'
+import img_hipper_freios from 'src/assets/images/marcas/hipper_freios.png'
+import img_kyb from 'src/assets/images/marcas/kyb.png'
+import img_magneti_marelli from 'src/assets/images/marcas/magneti_marelli.png'
+import img_man_filter from 'src/assets/images/marcas/man_filter.png'
+import img_monroe from 'src/assets/images/marcas/monroe.png'
+import img_nakata from 'src/assets/images/marcas/nakata.png'
+import img_tecfil from 'src/assets/images/marcas/tecfil.png'
+import img_trw from 'src/assets/images/marcas/trw.png'
+
 const imagens = ref([
-  { img: mahle, alt: 'Mahle' },
-  { img: mahle, alt: 'Mahle' },
-  { img: mahle, alt: 'Mahle' },
-  { img: mahle, alt: 'Mahle' },
-  { img: mahle, alt: 'Mahle' },
+  { img: img_mahle, alt: 'Mahle' },
+  { img: img_bosch, alt: 'Bosch' },
+  { img: img_cobreq, alt: 'Cobreq' },
+  { img: img_cofap, alt: 'Cofap' },
+  { img: img_controil, alt: 'Controil' },
+  { img: img_frasle, alt: 'Frasle' },
+  { img: img_fremax, alt: 'Fremax' },
+  { img: img_hipper_freios, alt: 'Hipper Freios' },
+  { img: img_kyb, alt: 'Kyb' },
+  { img: img_magneti_marelli, alt: 'Magneti Marelli' },
+  { img: img_man_filter, alt: 'Man Filter' },
+  { img: img_monroe, alt: 'Monroe' },
+  { img: img_nakata, alt: 'Nakata' },
+  { img: img_tecfil, alt: 'Tecfil' },
+  { img: img_trw, alt: 'Trw' },
 ])
 
 // Duplicar as imagens para o loop infinito parecer contínuo
 const imagensDuplicadas = computed(() => [...imagens.value, ...imagens.value])
-
 </script>
 
 <style scoped>
