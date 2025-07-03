1d6ad7e92fa76daa5464aad580d5ea621e88772a
Carolina Mattos <mattos.carolina98@gmail.com>
2025-07-03 16:49:31 -0400

    fix: hero

--- src/components/cmp_hero.vue
+++ src/components/cmp_hero.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="bg-hero" >
-    <div style="position: relative;"></div>
+  <div class="bg-hero">
+    <div class="row container text-white flex justify-center items-center  ">
+      <div class="col-xs-12 col-sm-6">
+        <p>Mais de 25 anos no mercado</p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda reprehenderit impedit
+          cupiditate illo porro consectetur atque dolore accusamus unde, beatae doloribus a rerum
+          rem earum! Nihil illum ad doloremque? Nulla!
+        </p>
+      </div>
+
+      <div class="col-xs-12 col-sm-6">
+        <p>Faça seu orçamento agora!</p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit recusandae itaque
+          eos libero sunt ut corrupti voluptatum. Illo ullam deleniti doloribus optio voluptatibus
+          totam cum vitae vel minus pariatur. Corporis?
+        </p>
+        <q-btn>Fazer orçamento > </q-btn>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 <style lang="scss" scoped>
 .bg-hero {
-  //padding: 20px;
-  //background-size: cover;
-  //background-position: center;
-  //background-repeat: no-repeat;
   position: relative;
-  //display: flex;
-  //align-items: flex-end;
-  //justify-content: center;
-  //opacity: 0.9;
-  //width: 100vw;
+  width: 100%;
+  background-color: $dark;
   min-height: 350px;
   height: auto;
 }
-.bg-hero::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: $dark;
-  z-index: 1;
-  pointer-events: none;
-}
+
 </style>
