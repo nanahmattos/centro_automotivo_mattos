07d41c9c10f6b515583bdb8b7291e4c5924329fd
Carolina Mattos <mattos.carolina98@gmail.com>
2025-07-09 15:49:10 -0400

    feat: configura build

--- quasar.config.js
+++ quasar.config.js
@@ -56,10 +56,11 @@ export default defineConfig((/* ctx */) => {
       // minify: false,
       // polyfillModulePreload: true,
       // distDir
+      distDir: 'dist/spa',
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
-      
+
       vitePlugins: [
         ['vite-plugin-checker', {
           eslint: {
